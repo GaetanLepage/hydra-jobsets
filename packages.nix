b87b130de4dc0c47eb9fd4b03a71a58de8a65c4a
Gaetan Lepage <gaetan@glepage.com>
2026-04-25 10:42:56 +0200

    more stuff

--- packages.nix
+++ packages.nix
@@ -9,24 +9,39 @@ in
 {
   topLevel = {
     uv = allSystems;
+    ruff = allSystems;
+    ty = allSystems;
+
     zed-editor = linuxSystems;
+    neovim = allSystems;
+
     nixl = linuxSystems;
     exo = allSystems;
     mujoco = allSystems;
   };
 
   python = {
-    jax = allSystems;
-    lineax = allSystems;
-
     torch = allSystems;
     torchaudio = allSystems;
     torchvision = allSystems;
     executorch = allSystems;
 
+    jax = allSystems;
+    equinox = allSystems;
+    lineax = allSystems;
+
     tinygrad = allSystems;
     dm-control = allSystems;
 
     fastexcel = allSystems;
+    lancedb = allSystems;
+
+    wandb = allSystems;
+
+    deep-gemm = linuxSystems;
+    deep-ep = linuxSystems;
+    nixl = linuxSystems;
+    xformers = linuxSystems;
+    cuda-bindings = linuxSystems;
   };
 }
